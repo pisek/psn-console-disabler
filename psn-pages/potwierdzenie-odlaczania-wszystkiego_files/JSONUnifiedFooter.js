9e9b75338dbe6fedeaf610d51e2505e936ca438d
Pisek <pisekfm@o2.pl>
2016-07-18 21:34:36 +0200

    pages examples

---
+++ psn-pages/potwierdzenie-odlaczania-wszystkiego_files/JSONUnifiedFooter.js
@@ -0,0 +1,110 @@
+SNEIUnifiedFooter._upfGetJsonResult({
+    "footer": [
+        {
+            "id":"servicesLinks",
+            "title":"Usługi",
+            "titleId":"header_Services",
+            "list":[
+            {
+                "link":
+                {
+                    "id":"footerLink_Music",
+                    "label":"Muzyka",
+                    "url":"http://www.playstationmusic.com/spotify"
+                }
+            },
+            {
+                "link":
+                {
+                    "id":"footerLink_PSN",
+                    "label":"Gry",
+                    "url":"https://store.sonyentertainmentnetwork.com/#!/pl-pl/home/gry"
+                }
+            }]
+        },
+        {
+            "id":"accountLinks",
+            "title":"Konto",
+            "titleId":"header_Account",
+            "list":[
+            {
+                "link":
+                {
+                    "id":"footerLink_CreateAnAccount",
+                    "label":"Utwórz konto",
+                    "url":"https://account.sonyentertainmentnetwork.com/reg/account/create-account!input.action?request_locale=pl_PL"
+                }
+            },
+            {
+                "link":
+                {
+                    "id":"footerLink_AccountManagement",
+                    "label":"Zarządzanie kontami",
+                    "url":"https://account.sonyentertainmentnetwork.com/cam/devices/device-list.action?request_locale=pl_PL"
+                }
+            }]
+        },
+        {
+            "id":"legalLinks",
+            "title":"Informacje prawne",
+            "titleId":"header_Legal",
+            "list":[
+            {
+                "link":
+                {
+                    "id":"footerLink_TermsOfService",
+                    "label":"Warunki świadczenia usługi",
+                    "url":"http://doc.dl.playstation.net/doc/legal/eu.html"
+                }
+            },
+            {
+                "link":
+                {
+                    "id":"footerLink_PrivacyPolicy",
+                    "label":"Pliki cookie i polityka prywatności",
+                    "url":"http://doc.dl.playstation.net/doc/legal/eu.html"
+                }
+            },
+            {
+                "link":
+                {
+                    "id":"footerLink_HealthWarnings",
+                    "label":"Ostrzeżenia dotyczące zdrowia",
+                    "url":"http://doc.dl.playstation.net/doc/legal/eu.html"
+                }
+            },
+            {
+                "link":
+                {
+                    "id":"footerLink_AboutUs",
+                    "label":"Skontaktuj się z nami",
+                    "url":"https://www.playstation.com/contactus/"
+                }
+            },
+            {
+                "link":
+                {
+                    "id":"footerLink_More",
+                    "label":"Więcej...",
+                    "url":"https://www.playstation.com/ageratings/"
+                }
+            }]
+        },
+        {
+            "id":"helpLinks",
+            "title":"Pomoc",
+            "titleId":"header_Help",
+            "list":[
+            {
+                "link":
+                {
+                    "id":"footerLink_CustomerSupport",
+                    "label":"Dział obsługi klienta",
+                    "url":"http://www.sonyentertainmentnetwork.com/pl-pl/support"
+                }
+            }]
+        }
+    ],
+    "browserWarningContent":"Dla komfortu korzystania z witryny Sony Entertainment Network, zaktualizuj przeglądarkę do najnowszej wersji. Kliknij <a id=\"browserTellMeMoreLink\" href=\"https://account.sonyentertainmentnetwork.com/\" target=\"_blank\">tutaj</a>, aby dowiedzieć się, jak to zrobić.",
+    "cookieWarningContent":"W witrynie stosowane są pliki cookie, aby zapewnić jak najlepsze wrażenia. Możesz zmienić swoje ustawienia plików cookie, aby wyłączyć ich obsługę. <a id=\"cookiesTellMeMoreLink\" href=\"http://en.support.sonyentertainmentnetwork.com/app/answers/detail/a_id/2981/\" target=\"_blank\">Powiedz mi więcej</a>."
+});
