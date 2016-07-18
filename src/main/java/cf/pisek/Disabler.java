5c3a814253beb7a82ca5abf293d1876492ea5b81
Pisek <pisekfm@o2.pl>
2016-07-18 21:33:51 +0200

    smal fixes

--- src/main/java/cf/pisek/Disabler.java
+++ src/main/java/cf/pisek/Disabler.java
@@ -1,14 +1,16 @@
 package cf.pisek;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 public class Disabler {
 
@@ -25,45 +27,73 @@ public class Disabler {
 		User user = null;
 		if (cmd.hasOption("u") && cmd.hasOption("p")) {
 			user = new User(cmd.getOptionValue("u"), cmd.getOptionValue("p"));
-		}
 		
-		if (cmd.hasOption("t")) {
-			// continously
-			int seconds = Integer.parseInt(cmd.getOptionValue("t"));
-			while (true) {
+			if (cmd.hasOption("t")) {
+				// continously
+				int seconds = Integer.parseInt(cmd.getOptionValue("t"));
+				while (true) {
+					
+					tryDisablingTheConsole(user);
+					
+					Thread.sleep(seconds * 1000);
+				}
+			} else {
+				// only once
 				
 				tryDisablingTheConsole(user);
 				
-				Thread.sleep(seconds * 1000);
 			}
-		} else {
-			// only once
 			
-			tryDisablingTheConsole(user);
+		} else {
+			
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("disabler", options );
 			
 		}
 		
 	}
 
 	private static void tryDisablingTheConsole(User user) throws IOException {
+		Response res;
+		Connection con;
 
-		Connection con = Jsoup.connect("https://account.sonyentertainmentnetwork.com/login.action?category=psn&displayNavigation=false");
+		con = Jsoup.connect("https://account.sonyentertainmentnetwork.com/login.action?category=psn&displayNavigation=false");
 		con.header("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36");
 		con.get();
 		
-		con.url("https://account.sonyentertainmentnetwork.com/liquid/j_spring_security_check");
+		res = con.response();
+//		System.out.println(Arrays.toString(res.cookies().entrySet().toArray()));
+		
+		con = Jsoup.connect("https://account.sonyentertainmentnetwork.com/liquid/j_spring_security_check");
+		con.cookies(res.cookies());
 		con.data("j_username", user.getUser());
 		con.data("j_password", user.getPassword());
 		con.data("service-entity", "np");
-		con.post();
+		System.out.println(con.post());
+		
+		res = con.response();
+//		System.out.println(Arrays.toString(res.cookies().entrySet().toArray()));
 		
 		//already logged in
 		
-		// go to https://account.sonyentertainmentnetwork.com/liquid/cam/account/devices/media-devices-confirm-deactivate.action
+		// GET https://account.sonyentertainmentnetwork.com/liquid/cam/account/devices/media-devices-confirm-deactivate.action
+		con = Jsoup.connect("https://account.sonyentertainmentnetwork.com/liquid/cam/account/devices/media-devices-confirm-deactivate.action");
+		con.cookies(res.cookies());
+		System.out.println(con.get());
+		
+		res = con.response();
+//		System.out.println(Arrays.toString(res.cookies().entrySet().toArray()));
 		
 		// sprawdzamy czy istnieje button pozwalajacy na wylaczenie konsol
 		
 		// odpalamy wylaczenie
+		
+		// POST https://account.sonyentertainmentnetwork.com/liquid/cam/devices/clear-domain.action
+		con = Jsoup.connect("https://account.sonyentertainmentnetwork.com/liquid/cam/devices/clear-domain.action");
+		con.cookies(res.cookies());
+		System.out.println(con.post());
+		
+		res = con.response();
 		
 	}
 
